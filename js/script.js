97e9a61f03e2fad330d05d6991d6636133ff7add
tripledan003 <danielelaudani@abacatania.it>
2024-05-07 19:01:21 +0200

    responsive pt2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 var swiper = new Swiper(".home-sliding-cards", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   centerSlide: true,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true,
   },
 });
